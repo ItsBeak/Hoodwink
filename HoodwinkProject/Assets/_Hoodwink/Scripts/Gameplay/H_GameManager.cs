a50b26878454468eefd4e3708fc3e55fc994a565
Tom Collyer <karzacinc@gmail.com>
2023-07-18 15:15:08 +1000

    Round System, Level Loading & Role Assignment

--- HoodwinkProject/Assets/_Hoodwink/Scripts/Gameplay/H_GameManager.cs
+++ HoodwinkProject/Assets/_Hoodwink/Scripts/Gameplay/H_GameManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using Mirror;
 using TMPro;
 using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class H_GameManager : NetworkBehaviour
 {
@@ -9,16 +11,46 @@ public class H_GameManager : NetworkBehaviour
 
     [Header("Game Data")]
     [HideInInspector] public string relayCode;
-    [HideInInspector] public List<H_PlayerBrain> serverPlayers;
 
-    [Header("Components")]
-    public TextMeshProUGUI relayCodeUI;
-    public TextMeshProUGUI pingDisplay;
+    [HideInInspector] public List<H_PlayerBrain> serverPlayers;
+    [HideInInspector] public List<H_PlayerBrain> roundPlayers;
+    [HideInInspector] public List<H_PlayerBrain> roundDeadPlayers;
+    [HideInInspector] public List<H_PlayerBrain> roundAgents;
+    [HideInInspector] public List<H_PlayerBrain> roundSpies;
+
+    public int debugSpyCount = 1;
+
+
+    [HideInInspector] public string chosenScene;
+    bool winConditionMet = false;
+    H_LevelData currentLevel;
+
+    [HideInInspector, SyncVar] public RoundStage currentRoundStage = RoundStage.Lobby;
+    [HideInInspector, SyncVar] public WinConditions winCondition;
+
+    [Header("Lobby Settings")]
+    public Transform[] lobbySpawns;
 
     [Header("Player Colours")]
     public AgentData[] agentData;
     public Color[] pantsColours, shoesColours;
     List<AgentData> availableAgents;
+
+    [Header("Map Pool")]
+    [Scene] public string[] maps;
+
+    [Header("Round Timer Settings")]
+    public float warmupLength;
+    [SyncVar] float warmupTimer;
+    public float roundLength;
+    [SyncVar] float roundTimer;
+    public float postGameLength;
+    [SyncVar] float postGameTimer;
+
+    [Header("Components")]
+    public TextMeshProUGUI relayCodeUI;
+    public TextMeshProUGUI pingDisplay;
+    public TextMeshProUGUI timerDisplay;
 
     [Header("Debugging")]
     public bool enableDebugLogs;
@@ -64,6 +96,90 @@ public class H_GameManager : NetworkBehaviour
     private void Update()
     {
         DisplayPing();
+
+        switch (currentRoundStage)
+        {
+            #region Lobby Stage Update
+            case RoundStage.Lobby:
+
+                timerDisplay.text = "Waiting for game to start...";
+
+                if (!isServer)
+                    return;
+
+                CheckReadyPlayers();
+
+                break;
+            #endregion
+
+            #region Warmup Stage Update
+            case RoundStage.Warmup:
+
+                timerDisplay.text = "Warmup ends in " + Mathf.RoundToInt(warmupTimer + 0.5f).ToString() + " seconds";
+
+                if (!isServer)
+                    return;
+
+                warmupTimer -= 1 * Time.deltaTime;
+
+                if (warmupTimer <= 0)
+                {
+                    currentRoundStage = RoundStage.Game;
+                    AssignRoles();
+                }
+
+                break;
+            #endregion
+
+            #region Game Stage Update
+            case RoundStage.Game:
+
+                timerDisplay.text = "In game, time left: " + Mathf.RoundToInt(roundTimer + 0.5f).ToString();
+
+                if (!isServer)
+                    return;
+
+                roundTimer -= 1 * Time.deltaTime;
+
+                if (winConditionMet)
+                {
+                    currentRoundStage = RoundStage.PostGame;
+                    //RpcShowRoundResults();
+                }
+
+                if (roundTimer <= 0)
+                {
+                    CheckWinConditions();
+                    currentRoundStage = RoundStage.PostGame;
+                    //RpcShowRoundResults();
+                }
+
+                break;
+            #endregion
+
+            #region Post Game Stage Update
+            case RoundStage.PostGame:
+
+                timerDisplay.text = "Game over, returning to lobby in : " + Mathf.RoundToInt(postGameTimer + 0.5f).ToString() + " seconds";
+
+                if (!isServer)
+                    return;
+
+                postGameTimer -= 1 * Time.deltaTime;
+
+                if (postGameTimer <= 0)
+                {
+                    ResetRoles();
+                    ResetPlayerStates();
+                    EndRound();
+                }
+
+                break;
+            #endregion
+
+            default: break;
+        }
+
     }
 
     [Command(requiresAuthority = false)]
@@ -94,6 +210,13 @@ public class H_GameManager : NetworkBehaviour
         availableAgents.Add(newAgentData);
 
         serverPlayers.Remove(player);
+        roundPlayers.Remove(player);
+        roundAgents.Remove(player);
+        roundSpies.Remove(player);
+        roundDeadPlayers.Remove(player);
+
+        CheckWinConditions();
+
     }
 
     void DisplayPing()
@@ -123,6 +246,202 @@ public class H_GameManager : NetworkBehaviour
 
         }
     }
+
+    [Server]
+    void CheckReadyPlayers()
+    {
+        bool allPlayersReady = true;
+
+        foreach (var player in serverPlayers)
+        {
+            if (!player.isReady)
+            {
+                allPlayersReady = false;
+                break;
+            }
+        }
+
+        if (allPlayersReady)
+        {
+            StartRound();
+        }
+
+    }
+
+    [Server]
+    public void StartRound()
+    {
+        StartCoroutine(InitializeLevel());
+
+        warmupTimer = warmupLength;
+        roundTimer = roundLength;
+        postGameTimer = postGameLength;
+
+        foreach (var player in serverPlayers)
+        {
+            roundPlayers.Add(player);
+            player.isReady = false;
+        }
+
+        ResetPlayerStates();
+        ResetRoles();
+
+        currentRoundStage = RoundStage.Warmup;
+
+    }
+
+    [Server]
+    public void EndRound()
+    {
+        foreach (var player in serverPlayers)
+        {
+            player.isReady = false;
+            //player.GetComponent<H_PlayerEquipment>().CmdDropItem();
+        }
+
+        foreach (var player in roundPlayers)
+        {
+            int randomSpawn = Random.Range(0, lobbySpawns.Length);
+
+            player.TeleportPlayer(lobbySpawns[randomSpawn].position, lobbySpawns[randomSpawn].rotation);
+        }
+
+        roundPlayers.Clear();
+        roundAgents.Clear();
+        roundSpies.Clear();
+
+        winConditionMet = false;
+
+        Invoke("CleanupObjects", 1f);
+
+        RpcUnloadMap(chosenScene);
+        currentRoundStage = RoundStage.Lobby;
+
+    }
+
+    IEnumerator InitializeLevel()
+    {
+        chosenScene = maps[Random.Range(0, maps.Length)];
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(chosenScene, LoadSceneMode.Additive);
+
+        while (!asyncLoad.isDone)
+        {
+            if (enableDebugLogs)
+                Debug.LogWarning("Loading Level: " + chosenScene);
+            yield return null;
+        }
+
+        if (enableDebugLogs)
+            Debug.LogWarning("Level Loaded: " + chosenScene);
+
+        RpcLoadMap(chosenScene);
+
+        yield return new WaitForSeconds(2);
+
+        currentLevel = FindObjectOfType<H_LevelData>();
+
+        Transform[] spawns = currentLevel.playerSpawnPoints;
+
+        foreach (var player in roundPlayers)
+        {
+            int randomSpawn = Random.Range(0, spawns.Length);
+
+            player.TeleportPlayer(spawns[randomSpawn].position, spawns[randomSpawn].rotation);
+        }
+
+
+    }
+
+    [Server]
+    private void AssignRoles()
+    {
+        int totalPlayers = roundPlayers.Count;
+        int spiesRemaining = debugSpyCount;
+
+        while (spiesRemaining > 0)
+        {
+            int randomPlayerIndex = Random.Range(0, totalPlayers);
+
+            if (roundPlayers[randomPlayerIndex].currentAlignment != AgentAlignment.Spy)
+            {
+                serverPlayers[randomPlayerIndex].currentAlignment = AgentAlignment.Spy;
+                roundSpies.Add(serverPlayers[randomPlayerIndex]);
+                spiesRemaining--;
+            }
+        }
+
+        for (int i = 0; i < totalPlayers; i++)
+        {
+            if (serverPlayers[i].currentAlignment != AgentAlignment.Spy)
+            {
+                serverPlayers[i].currentAlignment = AgentAlignment.Agent;
+                roundAgents.Add(serverPlayers[i]);
+            }
+        }
+
+    }
+
+    [ClientRpc]
+    void RpcLoadMap(string scene)
+    {
+        if (!isServer)
+            SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+    }
+
+    [ClientRpc]
+    void RpcUnloadMap(string scene)
+    {
+        SceneManager.UnloadSceneAsync(scene);
+    }
+
+    [Server]
+    private void ResetPlayerStates()
+    {
+        foreach (var player in serverPlayers)
+        {
+            //player.GetComponent<H_PlayerHealth>().isDead = false;
+            //player.GetComponent<H_PlayerHealth>().FullHeal();
+        }
+    }
+
+    [Server]
+    private void ResetRoles()
+    {
+        foreach (var player in serverPlayers)
+        {
+            player.currentAlignment = AgentAlignment.Unassigned;
+        }
+    }
+
+    [Server]
+    void CheckWinConditions()
+    {
+        if (winConditionMet)
+            return;
+
+        if (roundAgents.Count == 0 && debugSpyCount == 0)
+        {
+            winCondition = WinConditions.Draw;
+            winConditionMet = true;
+        }
+        else if (roundSpies.Count == 0)
+        {
+            winCondition = WinConditions.GoodWin;
+            winConditionMet = true;
+        }
+        else if (roundAgents.Count == 0)
+        {
+            winCondition = WinConditions.EvilWin;
+            winConditionMet = true;
+        }
+        else if (roundTimer <= 0)
+        {
+            winCondition = WinConditions.TimeOut;
+            winConditionMet = true;
+        }
+
+    }
 }
 
 [System.Serializable]
@@ -130,4 +449,20 @@ public struct AgentData
 {
     public string agentName;
     public Color agentColour;
+}
+
+public enum RoundStage
+{
+    Lobby,
+    Warmup,
+    Game,
+    PostGame,
+}
+
+public enum WinConditions
+{
+    GoodWin,
+    EvilWin,
+    TimeOut,
+    Draw
 }
---
+++ HoodwinkProject/Assets/_Hoodwink/Scripts/Gameplay/H_LevelData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class H_LevelData : MonoBehaviour
+{
+    public Transform[] playerSpawnPoints;
+}
