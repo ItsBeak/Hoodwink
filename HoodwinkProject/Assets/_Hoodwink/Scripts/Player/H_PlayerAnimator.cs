f0338c7584fd9e43d44e2a69702087767fe8b3c7
Tom Collyer <karzacinc@gmail.com>
2023-07-31 16:06:02 +1000

    Gun Idle & Backwards Walk Animations Implemented

--- HoodwinkProject/Assets/_Hoodwink/Scripts/Player/H_PlayerAnimator.cs
+++ HoodwinkProject/Assets/_Hoodwink/Scripts/Player/H_PlayerAnimator.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 public class H_PlayerAnimator : NetworkBehaviour
 {
     private H_PlayerController playerController;
+    private H_PlayerEquipment equipment;
     public Animator playerAnimator;
 
     void Start()
     {
         playerController = GetComponent<H_PlayerController>();
+        equipment = GetComponent<H_PlayerEquipment>();
     }
 
     void Update()
     {
+        playerAnimator.SetBool("isSidearm", equipment.currentSlot == EquipmentSlot.Sidearm);
+
+
         if (!isLocalPlayer)
             return;
 
