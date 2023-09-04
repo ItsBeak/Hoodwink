5e45eac210e4108b4144cde8636a833783dc7fdc
Tom Collyer <karzacinc@gmail.com>
2023-09-04 12:06:04 +1000

    Added Ability to Heal Others with Bandage Gadget

--- HoodwinkProject/Assets/_Hoodwink/Scripts/Gameplay/Gadgets/H_GadgetBandage.cs
+++ HoodwinkProject/Assets/_Hoodwink/Scripts/Gameplay/Gadgets/H_GadgetBandage.cs
@@ -13,6 +13,8 @@ public class H_GadgetBandage : H_GadgetBase
     [SerializeField] int healAmount;
     H_PlayerHealth playerHealth;
 
+    public LayerMask targetLayers;
+
     public override void CmdUseGadgetPrimary()
     {
         base.CmdUseGadgetPrimary();
@@ -21,5 +23,31 @@ public class H_GadgetBandage : H_GadgetBase
 
         Debug.Log("Heal");
         playerHealth.Heal(healAmount);
+    }
+
+    public override void CmdUseGadgetSecondary()
+    {
+        base.CmdUseGadgetSecondary();
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(equipment.playerCamera.transform.position, transform.forward, out hit, 2, targetLayers))
+        {
+
+            if (!hit.collider.transform.IsChildOf(equipment.transform))
+            {
+                var health = hit.collider.gameObject.GetComponentInParent<H_PlayerHealth>();
+
+                if (health)
+                {
+                    health.Heal(healAmount);
+                }
+            }
+            else
+            {
+                Debug.LogError("Player heal hit: " + hit.collider.gameObject.name + ", which is a child of this player. Check layermask settings on both the hit object and the target player");
+            }
+        }
+
     }
 }
