9d58e3cbbd3d5754e0fdbed9fed8733850fb34b2
Tom Collyer <karzacinc@gmail.com>
2023-11-06 10:10:39 +1100

    Fixed Punching While Dead

--- HoodwinkProject/Assets/H_PlayerPunch.cs
+++ HoodwinkProject/Assets/H_PlayerPunch.cs
@@ -19,11 +19,13 @@ public class H_PlayerPunch : NetworkBehaviour
     public BoxCollider damageCollider;
     H_PlayerAnimator animator;
     H_PlayerEquipment equipment;
+    H_PlayerHealth health;
 
     public void Start()
     {
         animator = GetComponent<H_PlayerAnimator>();
         equipment = GetComponent<H_PlayerEquipment>();
+        health = GetComponent<H_PlayerHealth>();
 
         attackTimer = attackCooldown;
         damageCollider.enabled = false;
@@ -37,7 +39,7 @@ public class H_PlayerPunch : NetworkBehaviour
         attackTimer -= Time.deltaTime;
         canAttack = attackTimer <= 0;
 
-        if (equipment.isPrimaryUseKeyPressed && equipment.currentSlot == EquipmentSlot.PrimaryItem && !equipment.primaryClientObject && canAttack)
+        if (equipment.isPrimaryUseKeyPressed && equipment.currentSlot == EquipmentSlot.PrimaryItem && !equipment.primaryClientObject && canAttack && !health.isDead)
         {
             if (H_GameManager.instance.currentRoundStage == RoundStage.Game)
             {
