e398c8dd7da6806090c786bf029b328d75e9916e
Tom Collyer <karzacinc@gmail.com>
2023-12-04 13:58:27 +1100

    Hud Hider Added (For Screenshots)

--- HoodwinkProject/Assets/H_GlobalHudHider.cs
+++ HoodwinkProject/Assets/H_GlobalHudHider.cs
@@ -6,13 +6,33 @@ public class H_GlobalHudHider : MonoBehaviour
 {
     public CanvasGroup[] huds;
 
+    bool localHide = false;
+    public bool localOnly;
+
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            localHide = !localHide;
+        }
+
         foreach (var hud in huds)
         {
-            if (H_GameManager.instance)
+            if (localOnly)
             {
-                hud.alpha = H_GameManager.instance.globalHideHud ? 0 : 1;
+                hud.alpha = localHide ? 0 : 1;
+            }
+            else if (H_GameManager.instance)
+            {
+                if (localHide)
+                {
+                    hud.alpha = 0;
+                }
+                else
+                {
+                    hud.alpha = H_GameManager.instance.globalHideHud ? 0 : 1;
+                }
+
             }
         }
     }
